f07743304eb042b3d081e0eb6ef6e768c0521267
Félix <felixrisc@gmail.com>
2021-01-16 23:36:12 -0600

    Commit Inicial

---
+++ hbs/helpers.js
@@ -0,0 +1,13 @@
+// helpers
+const hbs = require('hbs');
+
+hbs.registerHelper('getAnio',() => new Date().getFullYear());
+
+hbs.registerHelper('capitaliza',(texto) => {
+    let palabras = texto.split(' ');
+    palabras.forEach((palabra,idx) => {
+        palabras[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+    });
+
+    return palabras.join(' ');  
+})
